2affa5740cdac06a1112234166248193ed3b6a29
pellimelam <pellimelam01@gmail.com>
2026-03-14 00:44:51 +0530

    Create send_telegram.py

---
+++ engine/send_telegram.py
@@ -0,0 +1,11 @@
+# send_telegram.py
+import requests, os
+
+text=open("final.txt").read()
+
+requests.post(
+f"https://api.telegram.org/bot{os.environ['TELEGRAM_TOKEN']}/sendMessage",
+json={
+"chat_id":os.environ["TELEGRAM_CHAT_ID"],
+"text":text
+})
